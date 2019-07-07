dacddb2d60c71bc2a3601de1c95bf802b4bc8160
Olajobi Murtadore <42491828+mtdola@users.noreply.github.com>
2019-07-07 03:02:33 +0100

    Deleted main.js
    merged code into one file

--- Javascript/Keyless car/main.js
+++
@@ -1,8 +0,0 @@
-var age = prompt("enter your age");
-if (age === "18") {
-	alert("Congrats on your first year of driving! Drive safe.");
-} else if (age < "18") {
-	alert("Sorry, you're too young to drive. Powering off");
-} else {
-	alert("Welcome, powering on. Enjoy your ride!");
-}

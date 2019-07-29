140a5be328469114b2ced95c971ebb805b89f144
Murtadore <mtdolaj@gmail.com>
2019-07-29 21:58:21 +0100

    Linked JS and workig fine

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -1,5 +1,6 @@
 var css = document.querySelector("h3");
 var color1 = document.getElementById("#color1");
+var color2 = document.getElementById("#color2");
+
 console.log(color1);
-var color2 = document.querySelector("#color2");
 console.log(color2);

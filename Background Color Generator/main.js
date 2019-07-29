1e290640f45c2cb9c88e467a4c8974124f61124c
Murtadore <mtdolaj@gmail.com>
2019-07-29 22:11:03 +0100

    Hi

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -1,6 +1,8 @@
 var css = document.querySelector("h3");
-var color1 = document.getElementById("#color1");
-var color2 = document.getElementById("#color2");
+var color1 = document.querySelector("#color1");
+var color2 = document.querySelector("#color2");
 
+color1 = color1.value;
 console.log(color1);
+color2 = color2.value;
 console.log(color2);

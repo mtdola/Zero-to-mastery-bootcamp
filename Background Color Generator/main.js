fe82b793f920d1d3bd992024417eb7c24019783c
Murtadore <mtdolaj@gmail.com>
2019-07-29 21:55:39 +0100

    Javascript not yet working

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -1,1 +1,5 @@
-js  
+var css = document.querySelector("h3");
+var color1 = document.getElementById("#color1");
+console.log(color1);
+var color2 = document.querySelector("#color2");
+console.log(color2);

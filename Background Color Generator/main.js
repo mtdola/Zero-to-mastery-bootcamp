7ce2980f852e20fd9b40a0ce0d6e9c8d4f210937
Murtadore <mtdolaj@gmail.com>
2019-07-29 22:25:17 +0100

    Trying to sort JS file

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -2,7 +2,12 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 
-color1 = color1.value;
-console.log(color1);
 color2 = color2.value;
-console.log(color2);
+
+color1.addEventListener("input", function() {
+	console.log(color1.value);
+});
+
+color2.addEventListener("input", function() {
+	console.log(color2.value);
+});

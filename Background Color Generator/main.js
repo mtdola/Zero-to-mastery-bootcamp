a1a75e24efd90812d8b353615d0d6b24150fb005
Murtadore <mtdolaj@gmail.com>
2019-08-02 23:34:19 +0100

    css life

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -1,8 +1,8 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
+var color2 = document.querySelector("#colortwo");
 
-color2 = colortwo.value;
+color2 = color2.value;
 
 color1.addEventListener("input", function() {
 	console.log(color1.value);

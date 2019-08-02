8d15070f29d639f4f0ae8aaae8b66c53ef0c6a19
Murtadore <mtdolaj@gmail.com>
2019-08-02 23:45:36 +0100

    Input functions working fine now

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -1,13 +1,13 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#colortwo");
+var color2 = document.querySelector("#color2");
+var body = document.querySelector("#bodyColor")
 
-color2 = color2.value;
 
 color1.addEventListener("input", function() {
 	console.log(color1.value);
 });
 
-colortwo.addEventListener("input", function() {
-	console.log(colortwo.value);
+color2.addEventListener("input", function() {
+	console.log(color2.value);
 });

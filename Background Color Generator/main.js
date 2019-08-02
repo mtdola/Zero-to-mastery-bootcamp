bcddb80501bd5c1af19255c7a9ea274d722f2860
Murtadore <mtdolaj@gmail.com>
2019-08-02 23:31:35 +0100

    Working fine

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -2,7 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 
-color2 = color2.value;
+color2 = colortwo.value;
 
 color1.addEventListener("input", function() {
 	console.log(color1.value);

64a2f49ab63539bbcb76debe5b99f59960c8fee3
Murtadore <mtdolaj@gmail.com>
2019-08-03 06:35:31 +0100

    made changes

--- Background Color Generator/main.js
+++ Background Color Generator/main.js
@@ -3,11 +3,11 @@ var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.querySelector("#bodyColor")
 
+function linearGrad() {
+	body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+}
 
-color1.addEventListener("input", function() {
-	console.log(color1.value);
-});
+color1.addEventListener("input", linearGrad);
 
-color2.addEventListener("input", function() {
-	console.log(color2.value);
-});
+color2.addEventListener("input", linearGrad);
+
